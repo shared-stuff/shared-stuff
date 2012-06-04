14355518542361b3f0cdb4e0c798bed35a9874af
shybyte <ein@volloeko.de>
2012-06-04 20:17:06 +0200

    Fixed bug for shorten web address #4

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -14,3 +14,24 @@ describe('MyCtrl1', function(){
 });
 
 
+
+describe('AppController.needsUserLoggedIn', function(){
+    var needsUserLoggedIn = AppController.needsUserLoggedIn;
+    beforeEach(function(){
+
+    });
+
+    it('should need no user login for some pages', function() {
+        expect(needsUserLoggedIn('/login')).toBeFalsy()
+        expect(needsUserLoggedIn('/invitation/user@host.com/secret')).toBeFalsy()
+        expect(needsUserLoggedIn('/invitation/user@host.com')).toBeFalsy()
+        expect(needsUserLoggedIn('/user@host.com/secret')).toBeFalsy()
+        expect(needsUserLoggedIn('/user@host.com')).toBeFalsy()
+    });
+
+    it('should need user login for most pages', function() {
+        expect(needsUserLoggedIn('/other/user@host.com/secret')).toBeTruthy()
+        expect(needsUserLoggedIn('/other/user@host.com')).toBeTruthy()
+        expect(needsUserLoggedIn('/mystuff')).toBeTruthy()
+    });
+});
