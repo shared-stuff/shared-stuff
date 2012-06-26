1e5fb03a8fa4ed453a180c8db67bbbaeec222bdd
shybyte <ein@volloeko.de>
2012-06-26 09:53:41 +0200

    test.sh compiles coffeescript without help

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,9 @@
 
 BASE_DIR=`dirname $0`
 
+coffee -c -o app/js/gen app/js
+coffee -c -o test/unit/gen test/unit
+
 java -jar "$BASE_DIR/../test/lib/jstestdriver/JsTestDriver.jar" \
      --config "$BASE_DIR/../config/jsTestDriver.conf" \
      --basePath "$BASE_DIR/.." \
