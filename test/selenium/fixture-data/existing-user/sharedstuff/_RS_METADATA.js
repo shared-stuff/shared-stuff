7245117e49000aaa64301c06783410a81100ab73
shybyte <ein@volloeko.de>
2012-07-24 10:48:15 +0200

    Automated Acceptance Tests #21

---
+++ test/selenium/fixture-data/existing-user/sharedstuff/_RS_METADATA.js
@@ -0,0 +1,13 @@
+{
+  "myFriendsList": {
+    "data": "{\"items\":[{\"id\":\"1343115178168\",\"name\":\"Marco\",\"userAddress\":\"shybyte@5apps.com\",\"secret\":\"\"}]}", 
+    "mime-type": "text/plain;charset=UTF-8"
+  }, 
+  "_RS_METADATA.js": {
+    "bytes": 404
+  }, 
+  "myStuffList": {
+    "data": "{\"items\":[{\"title\":\"Private Example Stuff\",\"description\":\"Private Example Stuff Description\",\"visibility\":\"friends\",\"sharingTypes\":[\"rent\"],\"sharingDirection\":\"give\",\"categories\":\"\",\"link\":\"\",\"image\":\"\",\"id\":\"1343115103584\",\"created\":1343115103584,\"modified\":1343115103584},{\"title\":\"Public Example Stuff\",\"description\":\"Public Example Stuff Description\",\"visibility\":\"public\",\"sharingTypes\":[\"rent\"],\"sharingDirection\":\"give\",\"categories\":\"\",\"link\":\"\",\"image\":\"\",\"id\":\"1343115151930\",\"created\":1343115151930,\"modified\":1343115151930}]}", 
+    "mime-type": "text/plain;charset=UTF-8"
+  }
+}
